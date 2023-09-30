183c8303a88ce68305645baec94b2521c8333256
anis khalef <anis.khalef@duetodata.com>
2023-09-30 01:17:43 +0100

    latest

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,97 +1,93 @@
-import { Link, Navbar, Text} from '@nextui-org/react';
-import React from 'react';
-import {Box} from '../styles/box';
-import {BurguerButton} from './burguer-button';
-import {UserDropdown} from './user-dropdown';
-import {DarkModeSwitch} from './darkmodeswitch';
+import { Navbar } from "@nextui-org/react";
+import Link from "next/link";
+import React from "react";
+import { Box } from "../styles/box";
+import { BurguerButton } from "./burguer-button";
+import { UserDropdown } from "./user-dropdown";
+import { DarkModeSwitch } from "./darkmodeswitch";
 
 interface Props {
-   children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-export const NavbarWrapper = ({children}: Props) => {
-   const collapseItems = [
-      'Dashboard',
-      'Orders',
-      'Settings',
-      'Log Out',
-   ];
-   return (
-     <Box
-       css={{
-         position: "relative",
-         display: "flex",
-         flexDirection: "column",
-         flex: "1 1 auto",
-         overflowY: "auto",
-         overflowX: "hidden",
-       }}
-     >
-       <Navbar
-         isBordered
-         css={{
-           borderBottom: "1px solid $border",
-           justifyContent: "space-between",
-           width: "100%",
-           "@md": {
-             justifyContent: "space-between",
-           },
+export const NavbarWrapper = ({ children }: Props) => {
+  const collapseItems = ["Dashboard", "Orders", "Settings", "Log Out"];
+  return (
+    <Box
+      css={{
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        flex: "1 1 auto",
+        overflowY: "auto",
+        overflowX: "hidden",
+      }}
+    >
+      <Navbar
+        isBordered
+        css={{
+          borderBottom: "1px solid $border",
+          justifyContent: "space-between",
+          width: "100%",
+          "@md": {
+            justifyContent: "space-between",
+          },
 
-           "& .nextui-navbar-container": {
-             border: "none",
-             maxWidth: "100%",
+          "& .nextui-navbar-container": {
+            border: "none",
+            maxWidth: "100%",
 
-             gap: "$6",
-             "@md": {
-               justifyContent: "space-between",
-             },
-           },
-         }}
-       >
-         <Navbar.Content showIn="md">
-           <BurguerButton />
-         </Navbar.Content>
-         <Navbar.Content
-           hideIn={"md"}
-           css={{
-             width: "100%",
-           }}
-         >
-           Admin Dashboard
-         </Navbar.Content>
-         <Navbar.Content>
-           <Navbar.Content>
-             <DarkModeSwitch />
-           </Navbar.Content>
-           <Navbar.Content hideIn={"md"}>
-             <UserDropdown />
-           </Navbar.Content>
-         </Navbar.Content>
+            gap: "$6",
+            "@md": {
+              justifyContent: "space-between",
+            },
+          },
+        }}
+      >
+        <Navbar.Content showIn="md">
+          <BurguerButton />
+        </Navbar.Content>
+        <Navbar.Content
+          hideIn={"md"}
+          css={{
+            width: "100%",
+          }}
+        >
+          Admin Dashboard
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Content>
+            <DarkModeSwitch />
+          </Navbar.Content>
+          <Navbar.Content hideIn={"md"}>
+            <UserDropdown />
+          </Navbar.Content>
+        </Navbar.Content>
 
-         <Navbar.Collapse>
-           {collapseItems.map((item, index) => (
-             <Navbar.CollapseItem
-               key={item}
-               activeColor="success"
-               css={{
-                 color: index === collapseItems.length - 1 ? "$error" : "",
-               }}
-               isActive={index === 2}
-             >
-               <Link
-                 color="inherit"
-                 css={{
-                   minWidth: "100%",
-                 }}
-                 href="#"
-               >
-                 {item}
-               </Link>
-             </Navbar.CollapseItem>
-           ))}
-         </Navbar.Collapse>
-       </Navbar>
-       {children}
-     </Box>
-   );
+        <Navbar.Collapse>
+          {collapseItems.map((item, index) => (
+            <Navbar.CollapseItem
+              key={item}
+              activeColor="success"
+              css={{
+                color: index === collapseItems.length - 1 ? "$error" : "",
+              }}
+              isActive={index === 2}
+            >
+              <Link
+                color="inherit"
+                css={{
+                  minWidth: "100%",
+                }}
+                href="#"
+              >
+                {item}
+              </Link>
+            </Navbar.CollapseItem>
+          ))}
+        </Navbar.Collapse>
+      </Navbar>
+      {children}
+    </Box>
+  );
 };
